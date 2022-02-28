996f188a764db38ec005afc27118be785c78bacb
inclam-antoniomoya <antonio.moya@inclam.com>
2022-02-28 09:00:58 +0100

    Main page

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import ContactIcons from '../Contact/ContactIcons';
 
 const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
@@ -9,24 +8,26 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me.png`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Antonio Moya</h2>
+        <p><a href="mailto:antonio.moya.diez@gmail.com">antonio.moya.diez@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hola a todos. Mis amigos me llaman Toño y llevo más de 20 años en el mundo de las
+        ciencias del medio ambiente en general y la hidrología en particular.
+        En todo ese tiempo he pasado por todas las modas de ambos mundos. Desde los modelos
+        numéricos de simulación hasta el aprendizaje automático o la teledetección. Desde el
+        MVC hasta el DDD. Desde el ODBC hasta el NoSQL...<br />
+        Durante todo ese camino he trabajo en empresas tanto tecnológicas como de ingeniería clasica
+        y he pasado multitud de vicisitudes, desde dejar a toda España sin Intenet unas horas hasta
+        proyectos de investigación europeos. Mi intención con este sitio web es contar mis aventuras
+        y desventuras, desmitificar las tecnlogías novedosas y, si a alguien le puede servir,
+        explicar qué es para mi un informático &quot;bueno&quot;.
       </p>
       <ul className="actions">
         <li>
